cd0c3d74495f6901c8a30f63c82a26d311729fed
Santanu Bhattacharya <santattech@gmail.com>
2025-06-03 17:56:48 +0530

    feat: destination screen added

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,6 +1,6 @@
 class LocationConstants {
-  static const double HOME_LATITUDE = 22.6095924;
-  static const double HOME_LONGITUDE = 88.4292532;
+  static const double HOME_LATITUDE = 22.6094839;
+  static const double HOME_LONGITUDE = 88.4320596;
   
   // Distance threshold in meters (to account for GPS accuracy)
   static const double LOCATION_THRESHOLD = 50.0;
